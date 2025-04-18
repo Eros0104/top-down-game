15b774fd10ac5fb63a5f21520838dc78de159ce5
Eros Mendonça <erosmendonca@outlook.com>
2025-04-18 18:40:22 +0200

    chore: using brew alias

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # SDL2
-SDL_INCLUDE_PATH := /opt/homebrew/Cellar/sdl2/2.30.3/include
-SDL_LIB_PATH := /opt/homebrew/Cellar/sdl2/2.30.3/lib
+SDL_INCLUDE_PATH := /opt/homebrew/opt/sdl2/include
+SDL_LIB_PATH := /opt/homebrew/opt/sdl2/lib
 
 # SDL2_image
-SDL_IMAGE_INCLUDE_PATH := /opt/homebrew/Cellar/sdl2_image/2.8.2_1/include
-SDL_IMAGE_LIB_PATH := /opt/homebrew/Cellar/sdl2_image/2.8.2_1/lib
+SDL_IMAGE_INCLUDE_PATH := /opt/homebrew/opt/sdl2_image/include
+SDL_IMAGE_LIB_PATH := /opt/homebrew/opt/sdl2_image/lib
